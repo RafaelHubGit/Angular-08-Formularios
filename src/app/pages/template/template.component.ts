f6cc306a61e11f8c2f7df43b306ffff966e2ecb6
Rafael <rafael.nava.1403@gmail.com>
2020-03-17 15:26:46 -0600

    Primer commit

---
+++ src/app/pages/template/template.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
+
+@Component({
+  selector: 'app-template',
+  templateUrl: './template.component.html',
+  styleUrls: ['./template.component.css']
+})
+export class TemplateComponent implements OnInit {
+
+  usuario = {
+    nombre: 'Rafael', 
+    apellido: 'Nava', 
+    correo: 'rafael.nava@gmail.com'
+  }
+
+  constructor( private paisService: PaisService ) { }
+
+  ngOnInit() {
+
+    this.paisService.getPaises()
+    .subscribe( paises => {
+      console.log(paises);
+    });
+
+  }
+
+  guardar( forma: NgForm ){
+    console.log(forma);
+
+    if( forma.invalid ){
+
+      Object.values( forma.controls ).forEach( control => {
+        control.markAsTouched();
+      } )
+
+      return;
+    }
+
+    console.log(forma.value);
+  }
+
+}
