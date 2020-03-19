0418fc5ba018e5477dccacddc2d3e2146351bf1b
Rafael <rafael.nava.1403@gmail.com>
2020-03-19 15:15:45 -0600

    Formularios Reactivos 2

--- src/app/services/validadores.service.ts
+++ src/app/services/validadores.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
@@ -10,13 +10,28 @@ export class ValidadoresService {
 
   noHerrera( control: FormControl ): {[s:string]: boolean}{
 
-    if( control.value?.toLowerCase() === 'herrera' ){
+    if( control.value.toLowerCase() === 'herrera' ){
       return {
         noHerrera: true
       }
     }
 
     return null;
+  }
+
+  passwordsIguales( pass1Name: string, pass2Name: string){
+
+    return ( formGroup: FormGroup ) => {
+      const pass1Control = formGroup.controls[pass1Name];
+      const pass2Contro2 = formGroup.controls[pass2Name];
+
+      if( pass1Control.value === pass2Contro2 ){
+        pass2Contro2.setErrors(null);
+      }else{
+        pass2Contro2.setErrors({ noEsIgual: true });
+      }
+    }
+
   }
 
 
